d83019858288b25913d1739a4ea7e61f749e1da3
AndyDaMandy <maldonado.andy.m@gmail.com>
2022-11-13 10:58:07 -0500

    created user model

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  validates :username, uniqueness: true
+  #https://github.com/heartcombo/devise/wiki/How-To:-Allow-users-to-sign-in-with-something-other-than-their-email-address
+  #for troubleshooting username info
+end
